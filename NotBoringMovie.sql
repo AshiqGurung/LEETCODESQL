e2dbc67a71045cd05cfd2023c39af0af5ac1dce1
Ashiq Gurung123 <67871177+AshiqGurung@users.noreply.github.com>
2025-05-16 15:46:19 +0100

    Update NotBoringMovie.sql

--- NotBoringMovie.sql
+++ NotBoringMovie.sql
@@ -1,8 +1,9 @@
 -- https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
 
+-- USING SQL MOD FUNCTION TO RETURN REMAINDER
 
 SELECT  
 id, movie, description, rating
 FROM Cinema
-Where (id % 2 = 1) AND description <> 'boring'
+Where  MOD(id, 2) = 1 AND description <> 'boring'
 ORDER BY rating DESC
