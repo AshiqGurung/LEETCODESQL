11616178679cd6041b30962d5b9aaa1c8ed8290c
Ashiq Gurung123 <67871177+AshiqGurung@users.noreply.github.com>
2025-05-16 15:43:33 +0100

    Create NotBoringMovie.sql

---
+++ NotBoringMovie.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+SELECT  
+id, movie, description, rating
+FROM Cinema
+Where (id % 2 = 1) AND description <> 'boring'
+ORDER BY rating DESC
