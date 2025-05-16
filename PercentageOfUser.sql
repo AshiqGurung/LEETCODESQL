5ea949f09c977302e6cbe59fb0ea57b69a486b2f
Ashiq Gurung123 <67871177+AshiqGurung@users.noreply.github.com>
2025-05-16 17:02:28 +0100

    Create PercentageOfUser.sql

---
+++ PercentageOfUser.sql
@@ -0,0 +1,10 @@
+-- https://leetcode.com/problems/percentage-of-users-attended-a-contest/description/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT 
+contest_id,
+ROUND(COUNT(DISTINCT user_id) * 100.0 / (SELECT COUNT( user_id) FROM Users), 2) AS percentage
+FROM Register 
+Group BY contest_id
+ORDER BY 
+percentage DESC,
+contest_id ASC
